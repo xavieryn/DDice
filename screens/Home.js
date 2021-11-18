7f9ec7ceae427993deb5951b5ef77777e5b01a18
snow-hare <ryan@steinglass.com>
2021-11-18 10:43:03 -0800

    Revert "Revert "adds home screen to app""
    This reverts commit 6017cd661a696b0a3d58d31796ec67acf2b4f9f2.

---
+++ screens/Home.js
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
+
+
+const windowHeight = Dimensions.get('window').height;
+
+export default function Home() {
+  const [title, setTitle] = React.useState('Home');
+  return (
+    
+    <View style={styles.container}>
+      <View
+        style={{
+          backgroundColor: '#231942',
+          borderBottomEndRadius: 5,
+          borderBottomStartRadius: 5,
+        }}>
+        <Text style={styles.header}>{title}</Text>
+      </View>
+      <View style={styles.innerContainer}>
+      
+         
+        
+        
+        <TouchableOpacity style={styles.button}>
+          <Text>
+            Get Started
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 20,
+    backgroundColor: '#413768',
+    borderRadius: 10,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#9988A4',
+    paddingBottom: windowHeight / 20,
+    justifyContent: 'space-between',
+  }, 
+  header: {
+    margin: 24,
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#E6DEFC',
+  },
+  innerContainer: {
+    flex: 1,   
+    alignItems: 'center',
+    borderWidth: 1,
+    justifyContent: 'center',
+    padding: 10,
+  },
+ 
+});
