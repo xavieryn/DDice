6017cd661a696b0a3d58d31796ec67acf2b4f9f2
snow-hare <ryan@steinglass.com>
2021-11-18 08:49:06 -0800

    Revert "adds home screen to app"
    This reverts commit a2fd429099aa4547e53cb480798328a9fee028ca.

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ https://github.com/Templarian/MaterialDesign-React
 
 
 import * as React from 'react';
+<<<<<<< HEAD
 import { StyleSheet, View } from "react-native";
 import BottomNav from './components/BottomNav';
 import { createState } from "niue"; 
@@ -32,6 +33,37 @@ export const setState = _setState;
 
 const App = () => {
   
+=======
+import { BottomNavigation, Text } from 'react-native-paper';
+import create from './screens/create.js';
+import compare from './screens/compare.js';
+import './components/DDGlobal.js';
+
+const App = () => {
+  const [index, setIndex] = React.useState(2);
+
+  const [routes] = React.useState([
+    { key: 'spells', title: 'Spells', icon: 'fire', color: '#231942' },
+    { key: 'create', title: 'Create', icon: 'auto-fix', color: '#231942' },
+    { key: 'home', title: 'Home', icon: 'home', color: '#231942' },
+    {
+      key: 'chart',
+      title: 'Compare',
+      icon: 'chart-bell-curve',
+      color: '#231942',
+    },
+    { key: 'settings', title: 'Settings', icon: 'cog', color: '#231942' },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    spells: create,
+    create: create,
+    home: create,
+    chart: compare,
+    settings: create,
+  });
+
+>>>>>>> parent of a2fd429 (adds home screen to app)
   return (
     <View style={styles.container}>
       <BottomNav/>
