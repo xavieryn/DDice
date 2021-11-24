cd4c95fde058100bd7c2ea2aea9a4005b2edd5e4
xavieryn <61944157+xavieryn@users.noreply.github.com>
2021-11-24 14:02:40 -0800

    uses new global variable, cannot create new object yet

--- components/DDGlobal.js
+++ components/DDGlobal.js
@@ -9,13 +9,19 @@ import { createState, useState } from '@hookstate/core';
 //   ]
 // });
 
-const _spellsObj = createState({ 
-  items: [
-        {id: 0, text: 'Choose a spell'},
-  ]
-});
+const _spellsObj = createState( [
+  {id: 0, text: 'Choose a spell'},
+  {id: 1, text: 'Choose a spell'},
+  {id: 2, text: 'test'},
 
-export const PLSWORK = () => useState(_spellsObj);
+  ]
+);
+
+
+
+//export const PLSWORK = () => useState(_spellsObj)
+export default _spellsObj;
+//export default test;
 
 //export const useStore = _useStore;
 //export const setState = _setState;
