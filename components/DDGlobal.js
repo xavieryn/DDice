1dde1a15d22ceab9d7f92d356af4906f1552c987
xavieryn <61944157+xavieryn@users.noreply.github.com>
2021-11-25 13:25:25 -0800

    adds comment code to DDGlobal.js

--- components/DDGlobal.js
+++ components/DDGlobal.js
@@ -3,20 +3,12 @@
 import { v4 as uuidv4 } from 'uuid';
 import { createState, useState } from '@hookstate/core';
 
-
+// global object 
 const _spellsObj = createState( [
   {id: uuidv4(), text: 'Choose a spell'},
   {id: uuidv4(), text: 'Choose a spell'},
   {id: uuidv4(), text: 'test'},
-
   ]
 );
 
-
-
-//export const PLSWORK = () => useState(_spellsObj)
 export default _spellsObj;
-//export default test;
-
-//export const useStore = _useStore;
-//export const setState = _setState;
