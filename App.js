5ba33044b7658bafdc46cb30cbceb81b3056e052
xavieryn <61944157+xavieryn@users.noreply.github.com>
2021-12-09 12:01:40 -0800

    automatically adds starter spells

--- App.js
+++ App.js
@@ -9,12 +9,30 @@ https://github.com/Templarian/MaterialDesign-React
 */
 
 
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View } from "react-native";
 import BottomNav from './app/components/BottomNav';
+import AsyncStorageLib from '@react-native-async-storage/async-storage';
+import { checkForUpdateAsync } from 'expo-updates';
 
 const App = () => {
-  
+
+  useEffect ( () => {
+    check();
+  })
+  const check = async () => {
+    const result = await AsyncStorageLib.getItem('spellTest')
+    let check = [];
+    if (result !== null) check = JSON.parse(result);
+    const updatedCheck = check.filter(c => c.key !== 0);
+    if (updatedCheck.length < 1) pp();
+
+  }
+
+  const pp = async () => {
+    const starterSpell = [{key:0, text:'Choose a spell'}]
+    await AsyncStorageLib.setItem('spellTest', JSON.stringify(starterSpell))
+  }
   return (
     <View style={styles.container}>
       <BottomNav/>
